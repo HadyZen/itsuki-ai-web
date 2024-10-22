5281fe5ba9b797860d0dbd4b331dac6c907f383f
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-10-22 22:52:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 const { spawn } = require("child_process");
 
 function hadySaveng() {
-
-	const child = spawn(`node itsuki.js`, {
-
-		cwd: __dirname,
-
-		stdio: "inherit",
-
-		shell: true
-
-	});
+ const child = spawn(`npm install && node itsuki.js`, {
+	cwd: __dirname,
+	stdio: "inherit",
+	shell: true
+});
 
 	child.on("close", (code) => {
-
-		if (code == 2) {			
-			hadySaveng();
-		}
-	});
-
+if (code == 2) {			
+hadySaveng();
+}
+});
 }
 
 hadySaveng();
