f17e2f99484f86cc94028455df230e1bc8fb0bb2
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-10-22 22:48:59 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const { spawn } = require("child_process");
+
+function hadySaveng() {
+
+	const child = spawn(`node itsuki.js`, {
+
+		cwd: __dirname,
+
+		stdio: "inherit",
+
+		shell: true
+
+	});
+
+	child.on("close", (code) => {
+
+		if (code == 2) {			
+			hadySaveng();
+		}
+	});
+
+}
+
+hadySaveng();
