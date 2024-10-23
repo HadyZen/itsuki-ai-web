47199049b60fbc140a26836fad6fde93c0f7ed4e
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-10-23 19:53:56 +0700

    Update itsuki.js

--- itsuki.js
+++ itsuki.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'hady-zen', 'hadi.html')); 
+app.get('/', (req, res) => { 
+ res.sendFile(path.join(__dirname, 'hady-zen', 'hadi.html'));
 });
 
 const port = 3000;
