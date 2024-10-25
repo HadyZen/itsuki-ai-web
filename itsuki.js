f5ebbd3cb6fe225d11739f456a2454bc2376fa52
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-10-25 17:33:24 +0700

    Update itsuki.js

--- itsuki.js
+++ itsuki.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(3000, () => { });
+app.get('/', (req, res) => { 
+    res.sendFile(path.join(__dirname, 'hady-zen', 'hadi.html'));
 });
 
-app.get('/', (req, res) => { 
- res.sendFile(path.join(__dirname, 'hady-zen', 'hadi.html'));
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
