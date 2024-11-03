3ea3ef8793f98fe2ad13e604e9fd98c6b403fda5
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-11-03 14:12:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const { spawn } = require("child_process");
 
 function hadySaveng() {
- const child = spawn(`npm install express && npm start`, {
-	cwd: __dirname,
-	stdio: "inherit",
-	shell: true
+ const child = spawn(`npm start`, { 
+ cwd: __dirname,
+ stdio: "inherit",
+ shell: true
 });
 
-	child.on("close", (code) => {
+ child.on("close", (code) => {
 if (code == 2) {			
-hadySaveng();
-}
+ hadySaveng();
+ }
 });
 }
 
